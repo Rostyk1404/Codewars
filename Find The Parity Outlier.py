94873154b7fee9db795e1bbc6046fc1c1de9612d
ross <rostik_novosiadlo@ukr.net>
2019-07-08 20:03:21 +0300

    implemented few more things

---
+++ Find The Parity Outlier.py
@@ -0,0 +1,26 @@
+"""
+    You are given an array (which will have a length of at least 3, but could be very large) containing integers.
+    The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single
+    integer N. Write a method that takes the array as an argument and returns this "outlier" N.
+
+    Examples:
+
+        [2, 4, 0, 100, 4, 11, 2602, 36]
+        Should return: 11 (the only odd number)
+
+        [160, 3, 1719, 19, 11, 13, -21]
+        Should return: 160 (the only even number)
+
+    Sample Tests:
+
+        test.assert_equals(find_outlier([2, 4, 6, 8, 10, 3]), 3)
+        test.assert_equals(find_outlier([2, 4, 0, 100, 4, 11, 2602, 36]), 11)
+        test.assert_equals(find_outlier([160, 3, 1719, 19, 11, 13, -21]), 160)
+"""
+
+
+def find_outlier(integers):
+    even = list(x for x in integers if x % 2 == 0)
+    odd = list(x for x in integers if x % 2 != 0)
+
+    return odd[0] if len(odd) < len(even) else even
---
+++ Mumbling.py
@@ -0,0 +1,26 @@
+"""
+    This time no story, no theory. The examples below show you how to write function accum:
+
+    Examples:
+        accum("abcd") -> "A-Bb-Ccc-Dddd"
+        accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+        accum("cwAt") -> "C-Ww-Aaa-Tttt"
+
+    Sample Tests:
+
+        Test.describe("accum")
+        Test.it("Basic tests")
+        Test.assert_equals(accum("ZpglnRxqenU"), "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu")
+        Test.assert_equals(accum("NyffsGeyylB"), "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb")
+        Test.assert_equals(accum("MjtkuBovqrU"), "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu")
+        Test.assert_equals(accum("EvidjUnokmM"), "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm")
+        Test.assert_equals(accum("HbideVbxncC"), "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc")
+"""
+
+
+def accum(s):
+    new_list = []
+    counter = 0
+    for x in s:
+        new_list.append(x.upper() + (x.lower() * counter))
+    return "-".join(new_list)
