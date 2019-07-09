de6a455c516f27fbd695e7e67febf5456e0da908
ross <rostik_novosiadlo@ukr.net>
2019-07-09 12:38:01 +0300

    three more tasks

---
+++ Find numbers which are divisible by given number.py
@@ -0,0 +1,21 @@
+"""
+    Complete the function which takes two arguments and returns all numbers which are divisible by the given divisor.
+    First argument is an array of numbers and the second is the divisor.
+
+    Example:
+
+        divisible_by([1, 2, 3, 4, 5, 6], 2) == [2, 4, 6]
+
+    Sample Tests:
+
+        Test.describe("Fixed tests")
+        Test.assert_equals(divisible_by([1,2,3,4,5,6], 2), [2,4,6])
+        Test.assert_equals(divisible_by([1,2,3,4,5,6], 3), [3,6])
+        Test.assert_equals(divisible_by([0,1,2,3,4,5,6], 4), [0,4])
+        Test.assert_equals(divisible_by([0], 4), [0])
+        Test.assert_equals(divisible_by([1,3,5], 2), [])
+"""
+
+
+def divisible_by(numbers, divisor):
+    return list(x for x in numbers if x % divisor == 0)
