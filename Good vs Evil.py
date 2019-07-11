55445d8b308de18ecd2e4bf41d53bee325568b56
ross <rostik_novosiadlo@ukr.net>
2019-07-11 14:48:40 +0300

    added link for that tasks

--- Good vs Evil.py
+++ Good vs Evil.py
@@ -1,4 +1,8 @@
 """
+    Kata source :
+
+        https://www.codewars.com/kata/good-vs-evil/train/python/5d24b7020fb98b0028bd774b
+
     Description
     Middle Earth is about to go to war. The forces of good will have many battles with the forces of evil.
     Different races will certainly be involved. Each race has a certain worth when battling against others.
